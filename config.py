72b69990159fb66be487f58f35150e1b467f6381
gunal1916 <pcgunal19@gmail.com>
2025-09-05 11:28:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+alz-preventor-backend/   <-- Your main project folder
+└── app/                 <-- The 'app' folder
+    └── core/            <-- The 'core' folder inside 'app'
+        └── config.py    <-- The file you need to edit
